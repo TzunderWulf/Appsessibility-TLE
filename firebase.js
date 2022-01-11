926424e99c9b28f96db706aed221f8402abe2640
TzunderWulf <svandenbos@tzunder.nl>
2022-01-11 14:50:46 +0100

    Standard firebase config file added.

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export const db = getFirestore(app);
+
+export { auth };
